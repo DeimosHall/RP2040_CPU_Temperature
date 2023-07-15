e2157bb2e8d8dcd70ea12dc02c834dde37733f25
deimos <deimoshall@gmail.com>
2023-07-15 12:22:29 -0600

    feat: add example

---
+++ examples/Test/Test.ino
@@ -0,0 +1,22 @@
+/*
+ * Test sketch for the RP2040 CPU temperature sensor library.
+ * Author: Francisco Torres (deimoshall.dev)
+ * Date: July 2023
+ * License: MIT
+ * Repository: https://github.com/DeimosHall/RP2040_Temperature.git
+*/
+
+#include <CPU.h>  // Include the library to use the CPU temperature sensor
+
+CPU cpu;  // Create an instance of the CPU temperature sensor
+
+void setup() {
+  Serial.begin(9600);
+  cpu.begin();  // Initialize the CPU temperature sensor
+}
+
+void loop() {
+  // Print the CPU temperature in Celsius
+  Serial.println("CPU temperature: " + String(cpu.getTemperature()) + " °C");
+  delay(1000);
+}
